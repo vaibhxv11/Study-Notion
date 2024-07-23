7506ed16af62275c56b4e29a32a0e34552162123
Vaibhav D <vaibhavdudhal11@gmail.com>
2024-07-23 15:02:54 +0530

    init

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ const fileUpload=require("express-fileupload")
 const dotenv =require("dotenv");
 
 dotenv.config();
-const PORT =process.env.PORT || 4000 ;
+const PORT =process.env.PORT ;
 
 //database connect
 database.connect();
