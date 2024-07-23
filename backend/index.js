240498333ed7e4607a600ad7b48316551923f206
Vaibhav D <vaibhavdudhal11@gmail.com>
2024-07-23 14:54:10 +0530

    init

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,16 @@
 const express =require("express");
 const app=express();
 
-const userRoutes =require("../routes/User")
-const  profileRoutes =require("../routes/Profile")
-const paymentRoutes =require("../routes/Payments")
-const courseRoutes =require("../routes/Course")
-const contactUsRoute = require("../routes/Contact");
-const database =require("../config/database");
+const userRoutes =require("./routes/User")
+const  profileRoutes =require("./routes/Profile")
+const paymentRoutes =require("./routes/Payments")
+const courseRoutes =require("./routes/Course")
+const contactUsRoute = require("./routes/Contact");
+const database =require("./config/database");
 const cookieParser  =require("cookie-parser");
 //to connect with frontend
 const cors =require('cors')
-const {cloudinaryConnect} = require("../config/cloudinary")
+const {cloudinaryConnect} = require("./config/cloudinary")
 const fileUpload=require("express-fileupload")
 const dotenv =require("dotenv");
 
